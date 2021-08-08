b32b43ab29d27d44c1a717db3ca87781f1689e5f
Abu Jubaer <18-38784-3@student.aiub.edu>
2021-08-08 23:28:14 +0600

    Navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,82 @@
+//import { useState } from "react";
+import EmployeeNavbar from "./employeeComponents/EmployeeNavbar";
+//import { useFetch } from './employeeComponents/useFetch';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+        {/* <EmployeeNavbar /> */}
+        <Switch>
+            
+        <Route path="/employee">
+                    <EmployeeNavbar />
+                    <h2>Welcome to the  Employee Dashboard...</h2>
+        </Route>
+
+        <Route path="/employee/userList">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/packageAdd">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/package">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/placeAdd">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/place">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/gallery">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/galleryManage">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/salary">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/salary/List">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/satementAdd">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/statement">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/transactionAdd">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/transaction">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/advertisement">
+                    <EmployeeNavbar />
+        </Route>
+
+        <Route path="/employee/promo">
+                    <EmployeeNavbar />
+        </Route>
+
+        </Switch>
+    </Router>    
   );
 }
 
---
+++ frontend/src/employeeComponents/EmployeeNavbar.js
@@ -0,0 +1,32 @@
+import {Link} from 'react-router-dom';
+
+const EmployeeNavbar = ()=>{
+    return(
+        <div>
+            <Link to='/employee'>Dashboard</Link> |
+            <Link to='/employee/userlist'>Users</Link> |
+            <Link to='/employee/packageAdd'>Add Package</Link> |
+            <Link to='/employee/package'>Manage Package</Link> |
+            <Link to='/employee/placeAdd'>Add Place</Link> |
+            <Link to='/employee/place'>Manage Place</Link> |
+            <Link to='/employee/gallery'>Add Image</Link> |
+            <Link to='/employee/galleryManage'>Manage Album</Link> |
+            <Link to='/employee/salary'>Generate Salary</Link> |
+            <Link to='/employee/salary/List'>Salary List</Link> |
+            <Link to='/employee/statementAdd'>Add Income Statement</Link> |
+            <Link to='/employee/statement'>Income Statement</Link> |
+            <Link to='/employee/transactionAdd'>Add in Transaction History</Link> |
+            <Link to='/employee/transaction'>Transaction History</Link> |
+            <Link to='/employee/advertisement'>Advertisement</Link> |
+            <Link to='/employee/promo'>Send Promo</Link> |
+            <Link to='/employee/Review'>Review</Link> |
+            <Link to='/employee/support'>Support</Link> |
+            <Link to='/employee/guideline'>Guideline</Link> |
+            <Link to='/employee/faq'>FAQ</Link> |
+            <Link to='/employee/profile'>Profile</Link> |
+            <Link to='/login'>Logout</Link>
+        </div>
+    );
+}
+
+export default EmployeeNavbar;
